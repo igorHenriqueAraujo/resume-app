e7904d4cbdb38c0377663b8d48f587f9101d8aa4
Igor Henrique Araujo <igorha@primeinf.com.br>
2019-01-16 12:58:28 -0200

    Alterações para disponibilizar troca de idioma no menu mobile

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { TranslateService } from '@ngx-translate/core';
+import { Title } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-navbar',
@@ -7,9 +9,53 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NavbarComponent implements OnInit {
 
-  constructor() { }
+  private navbarCollapse: JQuery
+
+    //Inject dos services Translate para recursos i18n e Title para mudar o título dinâmicamente.
+    constructor(public translate: TranslateService, private titleService: Title) {
+      //adiciona idiomas suportados
+      translate.addLangs(['en', 'pt']);
+      //informa inglês como idioma default
+      translate.setDefaultLang('en');
+      //recupera o idioma do browser
+      const browserLang = translate.getBrowserLang();
+      //se o idioma do browser bater com português ou inglês de qualquer origem, informa aquele idioma,
+      //caso contrário mantém inglês como default.
+      translate.use(browserLang.match(/en|pt/) ? browserLang : 'en'); 
+      //informa o título dinamicamente de acordo com o idioma corrente.
+      this.setTitle(translate.currentLang);
+    }
 
   ngOnInit() {
+    this.navbarCollapse = $('.navbar-collapse');
+        // Fecha o menu responsivo quando o link do menu é acionado
+        $('.js-scroll-trigger').click(function() {
+          ($('.navbar-collapse') as any).collapse('hide');
+        });
+  }
+
+  
+  /**
+   * Método responsável por setar o título da aplicação dinamicamente de acordo com o idioma corrente.
+   * @param lang 
+   */
+  public setTitle( lang: string) {
+    
+    if (lang.match('pt')){
+      this.titleService.setTitle( 'Igor Henrique de Araújo - Perfil' );
+    } else {
+      this.titleService.setTitle( 'Igor Henrique de Araújo - Profile' );
+    }
+    
+  }
+
+  /**
+   * Método que troca o idioma corrente da aplicação.
+   * @param lang 
+   */
+  public changeLanguage( lang: string){
+    this.translate.use(lang);
+    this.setTitle(this.translate.currentLang);
   }
 
 }
